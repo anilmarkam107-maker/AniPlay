9bb7750f0d6af29915885d430d00a13fd6f4d48c
anilmarkam107-maker <anilmarkam107@gmail.com>
2026-09-21 10:30:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@
 # TOKEN = '4213543775:AsfjgfgdsdhfsdjhgjhfgsjgY'
 
 
-API_ID = ''
-API_HASH = ''
-TOKEN = ''
+API_ID = '33296771'
+API_HASH = 'e0e32b9aec22fd508a426496acfa4657'
+TOKEN = '8714627527:AAEyqBAjKmiOy0g9l7IPQmdQmRC0iA79t_Q'
